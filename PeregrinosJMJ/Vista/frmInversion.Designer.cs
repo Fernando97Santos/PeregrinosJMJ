e9d9352fa4c2fd1c4a12a36588e8795358a2f134
ferna <ferna@FERNANDOPC>
2018-04-01 21:53:04 -0600

    Prueba alfa xD

--- PeregrinosJMJ/Vista/frmInversion.Designer.cs
+++ PeregrinosJMJ/Vista/frmInversion.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmInversion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 319);
+            this.Name = "frmInversion";
             this.Text = "frmInversion";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
